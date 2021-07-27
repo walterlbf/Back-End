b175bd6f92e26085fe5a87d613d3ca841af8452d
walterlbf <walter.lbf.f@gmail.com>
2021-07-27 17:55:48 -0400

    trigger da movies_logs

--- 21.3/21.3 - triggers.sql
+++ 21.3/21.3 - triggers.sql
@@ -2,10 +2,18 @@ USE BeeMovies;
 DELIMITER $$
 
 CREATE TRIGGER trigger_movie_insert
-    BEFORE INSERT ON movies
+    BEFORE INSERT ON movies -- antes de inserir na tabela movies
+    FOR EACH ROW -- para cada linha
+BEGIN
+    SET NEW.release_year = YEAR(NOW()); -- set o ano
+END $$
+
+CREATE TRIGGER trigger_movie_log_insert
+    AFTER INSERT ON movies
     FOR EACH ROW
 BEGIN
-    SET NEW.release_year = YEAR(NOW());
+    INSERT INTO movies_logs(movie_id, executed_action, log_date)
+    VALUES(NEW.movie_id, 'INSERT', NOW());
 END $$
 
 DELIMITER ;
